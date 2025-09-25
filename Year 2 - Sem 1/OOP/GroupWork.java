692f232b5f2903fc11a010620e303f3747a2e4f9
Nathaniel Anzo Mugenyi <41405954+anzonathan@users.noreply.github.com>
2025-09-25 15:46:09 +0300

    Create GroupWork.java

---
+++ Year 2 - Sem 1/OOP/GroupWork.java
@@ -0,0 +1,102 @@
+// Group Work Excericse
+public class GroupWork{
+
+
+    public static void main(String[] args){
+        // Question 1
+        int score = 95;
+        double GPA = 3.85;
+        boolean isLoggedIn = false;
+
+        String username = "John_Doe";
+        System.out.println(username);
+
+        //Question 2
+
+        //1. long
+        //2. char
+        //3. Boolean
+        //4. double
+
+
+        //Question 3
+
+        int a = 10;
+        int b = 3;
+
+        double result = (double)a/b;
+
+        System.out.println(result);
+
+        // Value is 3.3333333333333335
+
+        double val = 45.78;
+
+        result = (int)val;
+
+        System.out.println(result);
+
+        //The result is 45.0
+
+        char inital = 'A';
+
+        result = (int)inital;
+
+        System.out.println(result);
+
+        //The result is 65.0 (ASCII no)
+
+        //Question 4
+
+        /*
+        * Primitive datatypes store data directly in memory but reference datatypes
+        * store references to the location where the actual object data is stored.
+        * */
+
+        //Question 5
+
+        //Statement 1 is 20
+        //Statement 2 is 5
+        //Statement 3 is 6
+        //Statement 4 is 0
+
+        //Question 6
+
+        // 1) True
+        // 2) True
+        // 3) False
+        // 4) True
+
+        //Question 7
+
+        int a1 = 20;
+        int b1 = 3;
+        int c1 = 4;
+
+        int answer = a1-b1 *c1/2+1;
+
+        System.out.println(answer);
+        //The answer is 15
+
+        /*In Java programming, an expression is a combination of variables, operators, and values
+        that evaluates to a single result. It can produce different types of outcomes, such as numbers,
+         characters, or Boolean values, depending on its components.
+
+        Example of an Integer Expression:
+
+        int result = 5 + 3 * 2;  // Evaluates to 11
+
+
+        This expression uses arithmetic operators and evaluates to an integer value.
+
+       Example of a Boolean Expression:
+
+        boolean isAdult = age >= 18;  // Evaluates to true if age is 18 or more
+
+
+        This expression uses a relational operator and evaluates to a Boolean
+        */
+
+
+    }
+}
