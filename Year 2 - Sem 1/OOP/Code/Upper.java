e27e3a45170df82815bce98e9fc4b06c887ed971
Nathaniel Anzo Mugenyi <41405954+anzonathan@users.noreply.github.com>
2025-10-05 18:54:37 +0300

    Create Upper.java

---
+++ Year 2 - Sem 1/OOP/Code/Upper.java
@@ -0,0 +1,15 @@
+public class Upper {
+
+    public static void main(String[] args){
+
+        //Uppercase
+        String Name = "Nathaniel";
+        String Caps = Name.toUpperCase();
+        System.out.println(Caps);
+
+        //Lowercase
+        String Friend = "Richard";
+        String noCaps = Friend.toLowerCase();
+        System.out.println(noCaps);
+    }
+}
