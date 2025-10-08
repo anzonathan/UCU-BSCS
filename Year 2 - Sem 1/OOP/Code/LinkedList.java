ad63e8bc74b760d4161b8592047dc4795209e6e2
Nathaniel Anzo Mugenyi <41405954+anzonathan@users.noreply.github.com>
2025-10-08 08:28:46 +0300

    Create LinkedList.java

---
+++ Year 2 - Sem 1/OOP/Code/LinkedList.java
@@ -0,0 +1,26 @@
+
+import java.util.LinkedList;
+
+public class linkedList {
+
+    //Main Driver Method
+    public static void main(String[] args){
+
+        //Creating a linked List
+
+        LinkedList<String> friends = new LinkedList<String>();
+
+        // Adding Elements to the LinkedLIst using add() method
+
+        friends.add("Charis");
+        friends.add("Andrea");
+        friends.add("Owen");
+        friends.add("Faith");
+        friends.add("Cole");
+
+
+        System.out.println(friends);
+
+
+    }
+}
